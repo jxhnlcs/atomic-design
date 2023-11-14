04e56b4ff465afb68033acd110219ad1057f0d4b
jxhnlcs <joohnzay@gmail.com>
2023-11-14 14:48:23 -0300

    (update): new atoms

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Inicio</h1>
+  <div class="component">
+    <Logo />
+    <NavItem text="Home" to="/home" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { Logo } from '@/components/atoms'
+import { NavItem } from '@/components/atoms'
 
-export default defineComponent({});
+export default defineComponent({
+  components: {Logo, NavItem}
+});
 </script>
+
+<style lang="scss" scoped>
+.component{
+  width: 300px;
+}
+</style>
